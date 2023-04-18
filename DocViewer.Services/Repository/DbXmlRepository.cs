99574bdbfc9a0a3fc9a352699802669f56953c98
lzlocki <lzlocki@poczta.fm>
2023-04-18 18:33:41 +0200

    Add repository for retriving data from xml database

---
+++ DocViewer.Services/Repository/DbXmlRepository.cs
@@ -0,0 +1,23 @@
+using System.Xml.Serialization;
+using DocViewer.Models.Models;
+using DocViewer.Services.Repository;
+
+namespace DocViewer.Services 
+{
+    public class DbXmlRepository 
+    {
+
+        
+        // READ
+        public Documents ReadDocumentsFromDatabaseByProductId(string ProductId) {
+            List<Documents> documentsList = new List<Documents>();
+            Documents? documents = new Documents();
+
+            // Loading database
+            documentsList = DbXmlEngine.LoadDatabase();
+            // Extracting data refering proper ProductId           
+            documents = documentsList.Where(id => id.ProductId == ProductId).FirstOrDefault();
+            return documents;
+        }
+    }
+}
