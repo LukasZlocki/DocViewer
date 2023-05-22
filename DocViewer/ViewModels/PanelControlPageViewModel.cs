c6cd72a00b61afda01f1e624a95a564451f30ae0
lzlocki <lzlocki@poczta.fm>
2023-05-22 22:29:40 +0200

    Add Vew Model for Control Panel control

---
+++ DocViewer/ViewModels/PanelControlPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DocViewer.ViewModels
+{
+    class PanelControlPageViewModel
+    {
+
+        public ICommand MoveDocumentsLeft { get; set; }
+
+        public ICommand MoveDocumentsRight { get; set; }
+
+
+        private void MoveDocumentsLeft()
+        {
+
+        }
+
+        private void MoveDocumentsRight()
+        {
+
+        }
+
+    }
+}
