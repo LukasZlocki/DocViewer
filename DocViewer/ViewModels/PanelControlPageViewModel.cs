962c3afaf77397a5bbbd25c77edf4e6c3066b298
lzlocki <lzlocki@poczta.fm>
2023-05-22 22:53:18 +0200

    Add fields for counter

--- DocViewer/ViewModels/PanelControlPageViewModel.cs
+++ DocViewer/ViewModels/PanelControlPageViewModel.cs
@@ -4,12 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace DocViewer.ViewModels
 {
     class PanelControlPageViewModel
     {
+
+        public int page { get; set; }
+        public int maxPages { get; set; }
+        string Counter { get; set; }
 
         public ICommand MoveDocumentsLeftCommand { get; set; }
 
@@ -24,12 +29,12 @@ namespace DocViewer.ViewModels
 
         private void MoveDocumentsLeft()
         {
-
+            // ToDo: code moving betwen documents here  
         }
 
         private void MoveDocumentsRight()
         {
-
+            // ToDo: code moving betwen documents here  
         }
 
     }
