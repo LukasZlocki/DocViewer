8b879c6707ce5ca8b8ee660ed1f58406284acd46
lzlocki <lzlocki@poczta.fm>
2023-08-07 20:46:46 +0200

    Remove unused usings

--- DocViewer/ViewModels/SettingsWindowViewModel.cs
+++ DocViewer/ViewModels/SettingsWindowViewModel.cs
@@ -1,14 +1,7 @@
 ﻿using DocViewer.Helpers;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
-using DocViewer;
 
 namespace DocViewer.ViewModels
 {
