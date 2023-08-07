6e188eb5649bea9cdf922490a3ec55241f2bb438
lzlocki <lzlocki@poczta.fm>
2023-08-07 19:13:10 +0200

    Binding properties for matrix path

---
+++ DocViewer/ViewModels/SettingsWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using DocViewer.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DocViewer.ViewModels
+{
+    class SettingsWindowViewModel :INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string _txtBoxMatrixPath;
+
+        public ICommand SaveSettingsCommand { get; set; }
+        public ICommand MatrycaPathTxtCommand { get; set; }
+
+        // constructor
+        public SettingsWindowViewModel()
+        {
+            SaveSettingsCommand = new RelayCommand(SaveSetttings);
+            MatrycaPathTxtCommand = new RelayCommand(SaveSetttings);
+        }
+
+
+        // this is productId
+        public string txtBoxMatrixPath
+        {
+            get { return _txtBoxMatrixPath; }
+            set
+            {
+                if (value != _txtBoxMatrixPath)
+                {
+                    _txtBoxMatrixPath = value;
+                    OnPropertyChanged("txtBoxMatrixPath");
+                }
+            }
+        }
+
+
+        private void SaveSetttings()
+        {
+            // ToDo: Write logic for saving matrix and document folder settings
+        }
+
+        protected void OnPropertyChanged(string fieldName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(fieldName));
+        }
+
+    }
+}
