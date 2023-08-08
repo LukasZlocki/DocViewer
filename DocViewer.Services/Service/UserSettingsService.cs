4b6f7f5760b71712e75934739597799d3756732d
lzlocki <lzlocki@poczta.fm>
2023-08-08 17:28:15 +0200

    Add empty interface and service for settings

---
+++ DocViewer.Services/Service/UserSettingsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocViewer.Services.Service
+{
+    public  class UserSettingsService : IUserSettingsService
+    {
+    }
+}
