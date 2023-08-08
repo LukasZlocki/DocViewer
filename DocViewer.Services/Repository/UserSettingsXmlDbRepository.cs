fbba401f6b8ffe8aca6e83211c7c4cb8f5f5f5aa
lzlocki <lzlocki@poczta.fm>
2023-08-08 17:37:38 +0200

    Add empty repo for handling data exchange with db

---
+++ DocViewer.Services/Repository/UserSettingsXmlDbRepository.cs
@@ -0,0 +1,17 @@
+﻿using DocViewer.Models.Models;
+
+namespace DocViewer.Services.Repository
+{
+    public class UserSettingsXmlDbRepositoryb : IUserSettingsXmlDbRepository
+    {
+        public UserSettings ReadUserSettingsFromDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveUserSettingsToDatabase(UserSettings settings)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
