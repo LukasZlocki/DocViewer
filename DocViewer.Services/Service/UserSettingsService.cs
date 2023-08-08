0c3acb10373533d005048b402059c0e92f39ad89
lzlocki <lzlocki@poczta.fm>
2023-08-08 17:44:37 +0200

    Add logic for user settings service

--- DocViewer.Services/Service/UserSettingsService.cs
+++ DocViewer.Services/Service/UserSettingsService.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DocViewer.Models.Models;
+using DocViewer.Services.Repository;
 
 namespace DocViewer.Services.Service
 {
-    public  class UserSettingsService : IUserSettingsService
+    public class UserSettingsService : IUserSettingsService
     {
+        private UserSettingsXmlDbRepository _dbReader = new UserSettingsXmlDbRepository();
+        public UserSettings GetUserSettings()
+        {
+            var settings = _dbReader.ReadUserSettingsFromDatabase();
+            return settings;
+        }
+
+        public void SaveUserSettings(UserSettings settings)
+        {
+            _dbReader.SaveUserSettingsToDatabase(settings);
+        }
     }
 }
