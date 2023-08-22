53dc1c3a7234458d6a9af1f5eea338cb71a7e23f
lzlocki <lzlocki@poczta.fm>
2023-08-22 20:57:55 +0200

    Code cleanup

--- DocViewer.Models/Models/UserSettings.cs
+++ DocViewer.Models/Models/UserSettings.cs
@@ -5,5 +5,7 @@
         // todo: add separate classes for this model as soon as more settings are needed (ex paths to documents, screen possitions, ...)
         public string? DocumentsPath { get; set; }
         public string? MatrixPath { get; set; }
+
+        public WindowPosition WindowPossition { get; set; }
     }
 }
