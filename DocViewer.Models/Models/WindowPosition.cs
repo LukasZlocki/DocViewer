ed19e0bbc38d03fe87127453398eb44347283c9e
lzlocki <lzlocki@poczta.fm>
2023-12-29 14:11:36 +0100

    Add window dimensions to model

--- DocViewer.Models/Models/WindowPosition.cs
+++ DocViewer.Models/Models/WindowPosition.cs
@@ -4,5 +4,7 @@
     {
         public double Xpos { get; set; }
         public double Ypos { get; set; }
+        public double HeightWin { get; set; }
+        public double WidthWin { get; set; }
     }
 }
